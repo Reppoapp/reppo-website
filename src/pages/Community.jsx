cd21563a17960480c7017978cb9819fc7ea1a18e
Reppoapp <kevin.bite@reppoapp.com>
2025-07-13 09:45:08 -0400

    Update Community.jsx

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react'
 import SocialProof from '../components/SocialProof'
-import TrustSignals from '../components/TrustSignals'
-import InlineCTA from '../components/InlineCTA'
 
 const Community = () => {
   // Ensure page starts at top when loaded - enhanced for reliability
@@ -39,18 +37,6 @@ const Community = () => {
     <div className="pt-16">
       {/* Social Proof - Build trust with testimonials and stats */}
       <SocialProof />
-      
-      {/* Trust Signals - Industry recognition and security */}
-      <TrustSignals />
-      
-      {/* CTA to join the community */}
-      <InlineCTA 
-        title="Join Our Elite Community"
-        subtitle="Connect with 47,000+ performance athletes transforming their limits"
-        primaryButtonText="Join Community"
-        size="large"
-        className="my-16"
-      />
     </div>
   )
 }
